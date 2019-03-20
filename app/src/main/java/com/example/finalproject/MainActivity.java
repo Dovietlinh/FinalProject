d64e136be1250de81da9c8d560a72a0ae363e835
VIET LINH <linhdv106@gmail.com>
2019-03-20 21:55:45 +0700

    ok

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //dung ơi
+        //gg
         //ok
         // hinh đây
     }
