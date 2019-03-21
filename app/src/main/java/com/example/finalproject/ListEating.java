a1d169b7088ffdaeb70e48c14a314408b307a600
VIET LINH <linhdv106@gmail.com>
2019-03-21 20:14:43 +0700

    ok

--- app/src/main/java/com/example/finalproject/ListEating.java
+++ app/src/main/java/com/example/finalproject/ListEating.java
@@ -1,7 +1,11 @@
 package com.example.finalproject;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.StrictMode;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -39,7 +43,32 @@ public class ListEating extends Activity {
         listViewEating = findViewById(R.id.listViewEating);
         searchView = findViewById(R.id.searchViewEating);
         productList = new ArrayList<>();
-        LoadAll();
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(ListEating.this,
+                Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(ListEating.this,
+                    Manifest.permission.INTERNET)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(ListEating.this,
+                        new String[]{Manifest.permission.INTERNET},
+                        1);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            LoadAll();
+        }
+
     }
 
     public void LoadAll() {
