c1eb955e26ee9b159d17c6ab74f7bd2a33a3ac0b
VIET LINH <linhdv106@gmail.com>
2019-03-21 16:41:51 +0700

    ok

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.finalproject;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    private Button btnMenu;
+    private Button btnRandom;
+    private Button btnAdd;
+    private Button btnExit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //dung ơi
-        //gg
-        //ok
-        // hinh đây
+        btnMenu=findViewById(R.id.btnMenu);
+        btnAdd=findViewById(R.id.btnAdd);
+        btnExit=findViewById(R.id.btnExit);
+        btnRandom=findViewById(R.id.btnRandom);
+
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,ListEating.class);
+                startActivity(intent);
+            }
+        });
     }
 }
