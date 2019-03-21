23a3da93827ea1fb32eda2efe12daadb9802688d
VIET LINH <linhdv106@gmail.com>
2019-03-21 22:05:10 +0700

    ok

--- app/src/main/java/com/example/finalproject/AddProduct.java
+++ app/src/main/java/com/example/finalproject/AddProduct.java
@@ -1,14 +1,106 @@
 package com.example.finalproject;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddProduct extends Activity {
+    private Spinner spnCategory;
+    int categoryID;
+    private EditText txtName;
 
+    private Uri filePath;
+    //Firebase
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
+    private final int PICK_IMAGE_REQUEST = 71;
+
+    private ImageView imageView;
+    private Button btnImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
+        spnCategory=findViewById(R.id.spnCategory);
+        txtName=findViewById(R.id.txtName);
+        imageView=findViewById(R.id.imageProduct);
+        btnImage=findViewById(R.id.btnChoose);
+        List<String> listCategory=new ArrayList<>();
+        listCategory.add("Hằng ngày");
+        listCategory.add("Ngày Lễ");
+        listCategory.add("Đặc sản miền Bắc");
+        listCategory.add("Đặc sản miền Trung");
+        listCategory.add("Đặc sản miền Nam");
+        listCategory.add("Dã Ngoại");
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,listCategory);
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+        spnCategory.setAdapter(adapter);
+
+        btnImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(AddProduct.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(AddProduct.this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    } else {
+                        ActivityCompat.requestPermissions(AddProduct.this,
+                                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                1);
+                    }
+                } else {
+                    chooseImage();
+                }
+            }
+        });
+
+    }
+    private void chooseImage(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void addProduct(View view){
+        categoryID=spnCategory.getSelectedItemPosition()+1;
     }
 }
