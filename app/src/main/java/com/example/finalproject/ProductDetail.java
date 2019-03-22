5d2921c955b29200fd3ff7da7c5513d72bd8067d
Xuan Hinh <xuanhinh97@gmail.com>
2019-03-22 21:02:02 +0700

    hinh

--- app/src/main/java/com/example/finalproject/ProductDetail.java
+++ app/src/main/java/com/example/finalproject/ProductDetail.java
@@ -56,7 +56,6 @@ public class ProductDetail extends Activity {
             txtTypeProduct.setText("Món khác");
         }
 
-
         btnRecipes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
