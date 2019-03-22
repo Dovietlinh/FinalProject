13d8ef35351bb3de4325e2e94512d85f537bacf1
Xuan Hinh <xuanhinh97@gmail.com>
2019-03-22 21:45:56 +0700

    hinh

--- app/src/main/java/com/example/finalproject/Adapter/AdapterListEating.java
+++ app/src/main/java/com/example/finalproject/Adapter/AdapterListEating.java
@@ -2,16 +2,25 @@ package com.example.finalproject.Adapter;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.finalproject.Entity.Product;
 import com.example.finalproject.ProductDetail;
 import com.example.finalproject.R;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
 
 public class AdapterListEating extends BaseAdapter {
@@ -50,7 +59,13 @@ public class AdapterListEating extends BaseAdapter {
             listEating = context.getLayoutInflater().inflate(layout, null);
         }
         TextView txtName = listEating.findViewById(R.id.txtName);
+        TextView txtDescription = listEating.findViewById(R.id.txtDescription);
+        ImageView imageView = listEating.findViewById(R.id.imageView);
+
         txtName.setText(productList.get(position).getpName());
+        txtDescription.setText(productList.get(position).getpDescription());
+        imageView.setImageBitmap(download_Image(productList.get(position).getpImage()));
+
         listEating.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,5 +78,22 @@ public class AdapterListEating extends BaseAdapter {
             listEating.setBackgroundColor(Color.LTGRAY);
         }
         return listEating;
+    }
+
+    public static Bitmap download_Image(String url) {
+        Bitmap bm = null;
+        try {
+            URL aURL = new URL(url);
+            URLConnection conn = aURL.openConnection();
+            conn.connect();
+            InputStream is = conn.getInputStream();
+            BufferedInputStream bis = new BufferedInputStream(is);
+            bm = BitmapFactory.decodeStream(bis);
+            bis.close();
+            is.close();
+        } catch (IOException e) {
+            Log.e("Hub","Error getting the image from server : " + e.getMessage());
+        }
+        return bm;
     }
 }
