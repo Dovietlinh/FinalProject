ecae565e706cba7a544e3105d1be9cb972a4dec9
VIET LINH <linhdv106@gmail.com>
2019-03-24 10:33:33 +0700

    ok

--- app/src/main/java/com/example/finalproject/AddProduct.java
+++ app/src/main/java/com/example/finalproject/AddProduct.java
@@ -55,7 +55,6 @@ public class AddProduct extends Activity {
     private final int PICK_IMAGE_REQUEST = 71;
 
     private ImageView imageView;
-    private Button btnImage;
     private Spinner spnType;
     private EditText txtDescription;
     @Override
@@ -69,7 +68,6 @@ public class AddProduct extends Activity {
         spnCategory=findViewById(R.id.spnCategory);
         txtName=findViewById(R.id.txtName);
         imageView=findViewById(R.id.imageProduct);
-        btnImage=findViewById(R.id.btnChoose);
         spnType=findViewById(R.id.spnType);
         txtDescription=findViewById(R.id.txtDescription);
         List<String> listCategory=new ArrayList<>();
@@ -83,7 +81,7 @@ public class AddProduct extends Activity {
         adapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
         spnCategory.setAdapter(adapter);
 
-        btnImage.setOnClickListener(new View.OnClickListener() {
+        imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (ContextCompat.checkSelfPermission(AddProduct.this,
@@ -189,13 +187,6 @@ public class AddProduct extends Activity {
             //vì hàm insertCatalog trả về kiểu int
             SoapPrimitive soapPrimitive= (SoapPrimitive)
                     envelope.getResponse();
-            //chuyển về int để kiểm tra insert thành công hay thất bại
-            //int ret=Integer.parseInt(soapPrimitive.toString());
-//            int ret=1;
-//            String msg="Insert Cate Successful";
-//            if(ret<=0)
-//                msg="Insert Cate Failed";
-//            Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
         }
         catch(Exception e)
         {
@@ -255,7 +246,7 @@ public class AddProduct extends Activity {
                                         }
                                     } else {
                                         if(product.getpName().isEmpty()){
-                                            Toast.makeText(AddProduct.this, "Input not empty", Toast.LENGTH_SHORT).show();
+                                            Toast.makeText(AddProduct.this, "Sorry", Toast.LENGTH_SHORT).show();
                                         }
                                         uploadProduct(product);
                                     }
